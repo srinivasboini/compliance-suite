636122b5c8358a0cf495717fb2e5bfc120c5b740
srinivas boini <srinivasboini7@gmail.com>
2024-02-24 15:14:15 +0800

    Added logs

--- commons/src/main/java/org/leha/commons/Role.java
+++ commons/src/main/java/org/leha/commons/Role.java
@@ -4,12 +4,14 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
 @AllArgsConstructor
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public enum Role {
 
     ROLE_ADMIN("APP.BUSN_ADMIN"),
