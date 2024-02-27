d7df77cb87337c4199d829046656078485cf2f93
srinivas boini <srinivasboini7@gmail.com>
2024-02-27 21:45:22 +0800

    corrected imports

--- presentation-service/src/main/java/org/leha/presentation/PresentationServiceApplication.java
+++ presentation-service/src/main/java/org/leha/presentation/PresentationServiceApplication.java
@@ -1,4 +1,4 @@
-package org.leha.compliance;
+package org.leha.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
